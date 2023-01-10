d032e1d8d8d63b9762f735332de895d1046331f6
samisa-abeysinghe <samisa.abeysinghe@gmail.com>
2023-01-10 06:52:39 +0530

    Added test data and command for activity attendanc

--- test/run_test.bat
+++ test/run_test.bat
@@ -1,4 +1,5 @@
 curl -X POST -H "Content-Type: application/json" -d @avinya_type.json http://localhost:9091/avinya_types
+curl -X POST -H "Content-Type: application/json" -d @activity_attendance.json http://localhost:9091/activity_attendance
 
 curl http://localhost:9091/avinya_types
 
